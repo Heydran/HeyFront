bbc520c86d2c7ce8c1c6616071e054a03b3ef20f
Guest 02 <guest02@alunos.sc.senac.br>
2022-08-19 21:51:57 -0300

    iniciando reactNative

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, TextInput, SafeAreaView, TouchableOpacity, View } from 'react-native'
+import { useState } from "react"
 
 export default function App() {
+  const [frutaAtual, setFrutaAtual] = useState("")
+  const [frutas, setFrutas] = useState(["oi","oi"])
+  const [msg, setMsg] = useState("")
+  const adicionar = (fruta) => {
+    fruta!= '' ? setFrutas([...frutas,frutaAtual],setFrutaAtual(""),setMsg("")):alert("oi")
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      
+      <View>{frutas.map((fruta,i)=><Text key={i}>{fruta}</Text>)}</View>
+      <TextInput
+      placeholder="Digite a fruta"
+      style = {styles.input}
+      onChangeText={setFrutaAtual}
+      value={frutaAtual}
+      autoFocus
+      
+      />
+      <TouchableOpacity style = {styles.touchable}
+      onPress={adicionar}>
+        <Text>Adicionar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +38,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderColor:"black",
+    borderWidth:1,
+    backgroundColor:"lightgray"
+    
+  },
+  touchable:{
+    backgroundColor:"#0ff",
+    borderWidth:1,
+    borderColor:"black",
+    padding:5
+
+  }
 });
